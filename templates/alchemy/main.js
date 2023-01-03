404450a2601065542915221331c0877c11d1be30
Peter Haider <peter.haider@zeyos.com>
2023-01-03 13:17:39 +0100

    Add Prism integration

--- templates/alchemy/main.js
+++ templates/alchemy/main.js
@@ -31,6 +31,9 @@ const showPageFromHash = (hash) => {
     } else {
       page.classList.add('hide');
     }
+    if ((typeof Prism != 'undefined') && ((typeof usePrismOnPageChange != 'undefined') && usePrismOnPageChange)) {
+      Prism.highlightAll();
+    }
   });
   return activeIndex;
 };
